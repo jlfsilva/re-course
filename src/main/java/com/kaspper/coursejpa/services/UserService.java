08b52a559bf6b53f388c4150b35674705d819686
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-03-02 17:37:22 -0300

    Service layer, component registration

---
+++ src/main/java/com/kaspper/coursejpa/services/UserService.java
@@ -0,0 +1,30 @@
+package com.kaspper.coursejpa.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaspper.coursejpa.entities.User;
+import com.kaspper.coursejpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		List<User> list = new ArrayList<>();
+		list = repository.findAll();
+		return list;
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
