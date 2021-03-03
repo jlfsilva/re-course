d1d7586a85b7e37179c0af2de68b712d3014e930
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-03-03 16:57:22 -0300

    Payment, one-to-one association

--- src/main/java/com/kaspper/coursejpa/config/TestConfig.java
+++ src/main/java/com/kaspper/coursejpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.kaspper.coursejpa.entities.Category;
 import com.kaspper.coursejpa.entities.Order;
 import com.kaspper.coursejpa.entities.OrderItem;
+import com.kaspper.coursejpa.entities.Payment;
 import com.kaspper.coursejpa.entities.Product;
 import com.kaspper.coursejpa.entities.User;
 import com.kaspper.coursejpa.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z") , o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 
