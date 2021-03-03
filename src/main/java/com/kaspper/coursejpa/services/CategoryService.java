b2a23777661ce7ce57db19da7d95489ec1aecdbf
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-03-03 10:19:21 -0300

    Category

---
+++ src/main/java/com/kaspper/coursejpa/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.kaspper.coursejpa.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaspper.coursejpa.entities.Category;
+import com.kaspper.coursejpa.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		List<Category> list = new ArrayList<>();
+		list = repository.findAll();
+		return list;
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
