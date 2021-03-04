08c45a6fd61fd60dd8501b678e2a7be714831ad1
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-03-04 10:09:14 -0300

    User update

--- src/main/java/com/kaspper/coursejpa/services/UserService.java
+++ src/main/java/com/kaspper/coursejpa/services/UserService.java
@@ -12,27 +12,39 @@ import com.kaspper.coursejpa.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		List<User> list = new ArrayList<>();
 		list = repository.findAll();
 		return list;
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
